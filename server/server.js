70ad09b312009332d36c88bf5c2cbe4e27279631
Gabriel Certeza <gabrielscerteza@gmail.com>
2017-03-14 18:54:08 -0700

    Allow connection to app on port 8000

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const app = require('./server-config');
+
+const port = 8000;
+
+app.listen(port);
+
+/* eslint-disable no-console */
+console.log(`Server now listening on port ${port}`);
+/* eslint-enable no-console */
+
