cec55fd6ef006dc0a32c010fdf0dd41fbe7fa7d8
Huan Chen <hchen.fred@gmail.com>
2017-03-15 16:04:40 -0700

    (feat)calendar displays months and dates

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,10 @@ module.exports = {
           presets: ['react', 'es2015'],
         },
       },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader',
+      },
     ],
   },
 };
