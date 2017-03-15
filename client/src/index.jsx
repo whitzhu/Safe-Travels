4c6ab1863365597a54f923630129ccbe879ec96f
billywon <billy.y.won@gmail.com>
2017-03-14 19:28:26 -0700

    asdf

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -11,20 +11,6 @@ class App extends React.Component {
     };
   }
 
-  // componentDidMount() {
-  //   $.ajax({
-  //     url: '/items', 
-  //     success: (data) => {
-  //       this.setState({
-  //         items: data
-  //       })
-  //     },
-  //     error: (err) => {
-  //       console.log('err', err);
-  //     }
-  //   });
-  // }
-
   render() {
     return (<div>
       <h1>Hello World!</h1>
