7794da3d9c7da2842231df7a2129eb238cebd61c
Huan Chen <hchen.fred@gmail.com>
2017-03-15 18:23:09 -0700

    (feat)refactoring calendar code

--- client/src/components/react-dates/calendar.jsx
+++ client/src/components/react-dates/calendar.jsx
@@ -7,113 +7,129 @@
  *   http://www.opensource.org/licenses/mit-license.php
  *   http://www.gnu.org/licenses/gpl.html
  */
+
 import React from 'react';
 
-var Calendar = React.createClass({
-    calc: function (year, month) {
-        if (this.state.selectedElement) {
-            if (this.state.selectedMonth != month || this.state.selectedYear != year) {
-                this.state.selectedElement.classList.remove('r-selected');
-            } else {
-                this.state.selectedElement.classList.add('r-selected');
-            }
-        }
-        return {
-            firstOfMonth: new Date(year, month, 1),
-            daysInMonth: new Date(year, month + 1, 0).getDate()
-        };
-    },
-    componentWillMount: function () {
-        this.setState(this.calc.call(null, this.state.year, this.state.month));
-    },
-    componentDidMount: function () {
-
-    },
-    componentDidUpdate: function (prevProps, prevState) {
-        if (this.props.onSelect && prevState.selectedDt != this.state.selectedDt) {
-            this.props.onSelect.call(this.getDOMNode(), this.state);
-        }
-    },
-    getInitialState: function () {
-        var date = new Date();
-        return {
-            year: date.getFullYear(),
-            month: date.getMonth(),
-            selectedYear: date.getFullYear(),
-            selectedMonth: date.getMonth(),
-            selectedDate: date.getDate(),
-            selectedDt: new Date(date.getFullYear(), date.getMonth(), date.getDate()),
-            startDay: 1,
-            weekNumbers: false,
-            minDate: this.props.minDate ? this.props.minDate : null,
-            disablePast: this.props.disablePast ? this.props.disablePast : false,
-            dayNames: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
-            monthNames: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-            monthNamesFull: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
-            firstOfMonth: null,
-            daysInMonth: null
-        };
-    },
-    getPrev: function () {
-        var state = {};
-        if (this.state.month > 0) {
-            state.month = this.state.month - 1;
-            state.year = this.state.year;
-        } else {
-            state.month = 11;
-            state.year = this.state.year - 1;
-        }
-        Object.assign(state, this.calc.call(null, state.year, state.month));
-        this.setState(state);
-    },
-    getNext: function () {
-        var state = {};
-        if (this.state.month < 11) {
-            state.month = this.state.month + 1;
-            state.year = this.state.year;
-        } else {
-            state.month = 0;
-            state.year = this.state.year + 1;
-        }
-        Object.assign(state, this.calc.call(null, state.year, state.month));
-        this.setState(state);
-    },
-    selectDate: function (year, month, date, element) {
-        if (this.state.selectedElement) {
-            this.state.selectedElement.classList.remove('r-selected');
-        }
-        element.target.classList.add('r-selected');
-        this.setState({
-            selectedYear: year,
-            selectedMonth: month,
-            selectedDate: date,
-            selectedDt: new Date(year, month, date),
-            selectedElement: element.target
-        });
-    },
-    render: function () {
-        return (
-            <div className="r-calendar">
-                <div className="r-inner">
-                    <Header monthNames={this.state.monthNamesFull} month={this.state.month} year={this.state.year} onPrev={this.getPrev} onNext={this.getNext} />
-                    <WeekDays dayNames={this.state.dayNames} startDay={this.state.startDay} weekNumbers={this.state.weekNumbers} />
-                    <MonthDates month={this.state.month} year={this.state.year} daysInMonth={this.state.daysInMonth} firstOfMonth={this.state.firstOfMonth} startDay={this.state.startDay} onSelect={this.selectDate} weekNumbers={this.state.weekNumbers} disablePast={this.state.disablePast} minDate={this.state.minDate} />
-                </div>
-            </div>
-        );
+const Calendar = React.createClass({
+  calc: function (year, month) {
+    if (this.state.selectedElement) {
+      if (this.state.selectedMonth !== month || this.state.selectedYear !== year) {
+        this.state.selectedElement.classList.remove('r-selected');
+      } else {
+        this.state.selectedElement.classList.add('r-selected');
+      }
     }
+    return {
+      firstOfMonth: new Date(year, month, 1),
+      daysInMonth: new Date(year, month + 1, 0).getDate(),
+    };
+  },
+
+
+  componentWillMount: function () {
+    this.setState(this.calc.call(null, this.state.year, this.state.month));
+  },
+  componentDidUpdate: function (prevProps, prevState) {
+    if (this.props.onSelect && prevState.selectedDt !== this.state.selectedDt) {
+      this.props.onSelect.call(this.getDOMNode(), this.state);
+    }
+  },
+
+  getInitialState: function () {
+    const date = new Date();
+    return {
+      year: date.getFullYear(),
+      month: date.getMonth(),
+      selectedYear: date.getFullYear(),
+      selectedMonth: date.getMonth(),
+      selectedDate: date.getDate(),
+      selectedDt: new Date(date.getFullYear(), date.getMonth(), date.getDate()),
+      startDay: 1,
+      weekNumbers: false,
+      minDate: this.props.minDate ? this.props.minDate : null,
+      disablePast: this.props.disablePast ? this.props.disablePast : false,
+      dayNames: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
+      monthNames: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+      monthNamesFull: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+      firstOfMonth: null,
+      daysInMonth: null,
+    };
+  },
+
+  getPrev: function () {
+    const state = {};
+    if (this.state.month > 0) {
+      state.month = this.state.month - 1;
+      state.year = this.state.year;
+    } else {
+      state.month = 11;
+      state.year = this.state.year - 1;
+    }
+    Object.assign(state, this.calc.call(null, state.year, state.month));
+    this.setState(state);
+  },
+
+  getNext: function () {
+    const state = {};
+    if (this.state.month < 11) {
+      state.month = this.state.month + 1;
+      state.year = this.state.year;
+    } else {
+      state.month = 0;
+      state.year = this.state.year + 1;
+    }
+    Object.assign(state, this.calc.call(null, state.year, state.month));
+    this.setState(state);
+  },
+
+  selectDate: function (year, month, date, element) {
+    if (this.state.selectedElement) {
+        this.state.selectedElement.classList.remove('r-selected');
+    }
+    element.target.classList.add('r-selected');
+    console.log(year);
+    this.setState({
+      selectedYear: year,
+      selectedMonth: month,
+      selectedDate: date,
+      selectedDt: new Date(year, month, date),
+      selectedElement: element.target,
+    });
+  },
+  render: function () {
+    return (
+      <div className="r-calendar">
+        <div className="r-inner">
+            <Header monthNames={this.state.monthNamesFull} month={this.state.month} year={this.state.year} onPrev={this.getPrev} onNext={this.getNext} />
+            <WeekDays dayNames={this.state.dayNames} startDay={this.state.startDay} weekNumbers={this.state.weekNumbers} />
+            <MonthDates month={this.state.month} year={this.state.year} daysInMonth={this.state.daysInMonth} firstOfMonth={this.state.firstOfMonth} startDay={this.state.startDay} onSelect={this.selectDate} weekNumbers={this.state.weekNumbers} disablePast={this.state.disablePast} minDate={this.state.minDate} />
+        </div>
+      </div>
+    );
+  },
 });
 
-var Header = React.createClass({
-    render: function () {
-        return (
-            <div className="r-row r-head">
-                <div className="r-cell r-prev" onClick={this.props.onPrev.bind(this)} role="button" tabIndex="0"></div>
-                <div className="r-cell r-title">{this.props.monthNames[this.props.month]}&nbsp;{this.props.year}</div>
-                <div className="r-cell r-next" onClick={this.props.onNext.bind(this)} role="button" tabIndex="0"></div>
-            </div>
-        );
-    }
+// const Header = (props) => {
+//   return (
+//     <div className="r-row r-head">
+//       <div className="r-cell r-prev" onClick={props.onPrev.bind(this)} role="button" tabIndex="0"></div>
+//       <div className="r-cell r-title">{props.monthNames[props.month]}&nbsp;{props.year}</div>
+//       <div className="r-cell r-next" onClick={props.onNext.bind(this)} role="button" tabIndex="0"></div>
+//     </div>
+//   );
+// };
+
+
+const Header = React.createClass({
+  render: function () {
+    return (
+      <div className="r-row r-head">
+        <div className="r-cell r-prev" onClick={this.props.onPrev.bind(this)} role="button" tabIndex="0"></div>
+        <div className="r-cell r-title">{this.props.monthNames[this.props.month]}&nbsp;{this.props.year}</div>
+        <div className="r-cell r-next" onClick={this.props.onNext.bind(this)} role="button" tabIndex="0"></div>
+      </div>
+    );
+  },
 });
 
 var WeekDays = React.createClass({
@@ -139,13 +155,13 @@ var WeekDays = React.createClass({
     }
 });
 
-var MonthDates = React.createClass({
-    statics: {
-        year: new Date().getFullYear(),
-        month: new Date().getMonth(),
-        date: new Date().getDate(),
-        today: new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate())
-    },
+const MonthDates = React.createClass({
+  statics: {
+    year: new Date().getFullYear(),
+    month: new Date().getMonth(),
+    date: new Date().getDate(),
+    today: new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate()),
+  },
     render: function () {
         var haystack, day, d, current, onClick,
             isDate, className,
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,3 +1,4 @@
+import './css/calendar.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route } from 'react-router-dom';
