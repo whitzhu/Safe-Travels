3ee0105964bb56b0fc6463d596b494c1f3633b17
Gabriel Certeza <gabrielscerteza@gmail.com>
2017-03-15 17:32:51 -0700

    (Fix) Change port to work on both heroku and local

--- server/server.js
+++ server/server.js
@@ -2,9 +2,8 @@ const app = require('./server-config');
 
 const port = 8000;
 
-app.listen(port);
-
-/* eslint-disable no-console */
-console.log(`Server now listening on port ${port}`);
-/* eslint-enable no-console */
-
+app.listen(process.env.PORT || port, () => {
+  /* eslint-disable no-console */
+  console.log(`Server now listening on port ${port}`);
+  /* eslint-enable no-console */
+});
