b39ebef2961c7c62097b1416fa6247503d049b6e
Gabriel Certeza <gabrielscerteza@gmail.com>
2017-03-19 10:22:53 -0700

    (Refactor) Capitalize imports (#17)

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route } from 'react-router-dom';
-
-import $ from 'jquery';
-import Entrance from './components/entrance';
-import main from './components/main';
-import login from './components/login';
-import createHistory from '../../node_modules/history/createBrowserHistory';
+import Entrance from './components/Entrance';
+import Main from './components/Main';
+import Login from './components/Login';
+import CreateHistory from '../../node_modules/history/createBrowserHistory';
 import './css/calendar.css';
 
 // import dummyYelpAttractionData from '../dummyYelpAttractionData.js';
 // import dummyYelpRestaurantData from '../dummyYelpRestaurantData.js';
 
-const history = createHistory();
+const history = CreateHistory();
 
 
 class App extends React.Component {
