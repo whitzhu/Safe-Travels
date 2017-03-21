da1c91878622f1ca3466df40d442b87f7c9a2848
Gabriel Certeza <gabrielscerteza@gmail.com>
2017-03-21 15:46:59 -0700

    (feat) add saveDestination function

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Axios from 'axios';
 import Weather from './Weather';
 import AttractionList from './AttractionList';
 import RestaurantList from './RestaurantList';
 import GoogleMap from './GoogleMap';
 
 class Main extends React.Component {
+
+  saveDestination(destination) {
+    console.log('destination:', destination);
+    Axios.post('/saveTrip', {
+      destination: destination,
+    })
+    .then((res) => {
+      console.log('response from /saveTrip', res);
+    })
+    .catch((error) => {
+      console.log('error from /saveTrip', error);
+    });
+  }
 
   render() {
     console.log('queried location, ', this.props.location);
@@ -20,16 +34,18 @@ class Main extends React.Component {
           <div className="row">
             <h2>Hungry?</h2>
             <RestaurantList
+              saveDestination={this.saveDestination}
               restaurants={this.props.restaurantResults.businesses || []}
               selectDestination={this.props.selectDestination}
               queryYelp={this.props.queryYelp}
-            />    
+            />
           </div>
           <div className="row">
             <h2>Check out some of these attractions!</h2>
             <AttractionList
+              saveDestination={this.saveDestination}
               attractions={this.props.attractionResults.businesses || []}
-              selectDestination={this.props.selectDestination}  
+              selectDestination={this.props.selectDestination}
             />
           </div>
           <div className="row">
