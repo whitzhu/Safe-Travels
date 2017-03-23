621c1797f430a3f68ca1113c64c14dc346de7d83
Jason An <an.jason.1994@gmail.com>
2017-03-22 18:53:21 -0700

    (bug) Invoke destination function

--- client/src/components/SelectDestinationButton.jsx
+++ client/src/components/SelectDestinationButton.jsx
@@ -10,7 +10,6 @@ class SelectDestinationButton extends React.Component {
   }
 
   onClick() {
-    console.log(this.props)
     this.props.saveDestination(this.props.destination);
     this.props.selectDestination(this.props.destination);
     this.setState({
