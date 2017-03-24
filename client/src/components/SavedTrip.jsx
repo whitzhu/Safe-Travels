00fb2bd865d449bab8117df81c03f3f7aa6541af
Gabriel Certeza <gabrielscerteza@gmail.com>
2017-03-24 14:01:20 -0700

    (refactor) add PropTypes to SavedTrip.jsx and clean structure

--- client/src/components/SavedTrip.jsx
+++ client/src/components/SavedTrip.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+
+const propTypes = {
+  trip: PropTypes.object.isRequired,
+};
 
 const SavedTrip = (props) => {
   const trip = props.trip;
-  console.log(trip);
   return (
     <div>
       <div className="restaurant-list-entry col-md-4">
         <div className="card card-block">
-          <img className="rounded card-img-top yelp-img" src={trip.imageUrl} alt="picture of a restaurant" />
+          <img className="rounded card-img-top yelp-img" src={trip.imageUrl} alt="restaurant" />
           <div>
             <h4 className="card-title restaurant-li">
               <a href={trip.informationUrl} >{trip.name}</a>
@@ -24,4 +27,5 @@ const SavedTrip = (props) => {
   );
 };
 
+SavedTrip.propTypes = propTypes;
 export default SavedTrip;
