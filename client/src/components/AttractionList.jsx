ae92c3fc155bea2c780911a47371309756970e47
Gabriel Certeza <gabrielscerteza@gmail.com>
2017-03-24 12:54:13 -0700

    (refactor) add props validation

--- client/src/components/AttractionList.jsx
+++ client/src/components/AttractionList.jsx
@@ -1,26 +1,24 @@
-import React from 'react';
-
+import React, { PropTypes } from 'react';
 import AttractionListEntry from './AttractionListEntry';
 
+const propTypes = {
+  attractions: PropTypes.array.isRequired,
+  saveDestination: PropTypes.func.isRequired,
+};
 
-class AttractionList extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+const AttractionList = props => (
+  <div className="yelp-results-attractions">
+    <ol>
+      {props.attractions.map(value =>
+        <AttractionListEntry
+          attraction={value}
+          selectDestination={props.selectDestination}
+          saveDestination={props.saveDestination}
+        />,
+      )}
+    </ol>
+  </div>
+);
 
-  render() {
-    return (
-      <div className="yelp-results-attractions">
-        <ol>
-          {this.props.attractions.map(value =>
-            <AttractionListEntry attraction={value} selectDestination={this.props.selectDestination}
-              saveDestination={this.props.saveDestination}
-            />,
-          )}
-        </ol>
-      </div>
-    );
-  }
-}
-
+AttractionList.propTypes = propTypes;
 export default AttractionList;
