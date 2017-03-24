8082233b3a6c87bd1c0a97158179e173e7156bb2
Gabriel Certeza <gabrielscerteza@gmail.com>
2017-03-24 14:15:02 -0700

    (refactor) change ES6 class to stateless functional for RestaurantList.jsx

--- client/src/components/RestaurantList.jsx
+++ client/src/components/RestaurantList.jsx
@@ -1,48 +1,43 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import RestaurantListEntry from './RestaurantListEntry';
 
+const propTypes = {
 
-class RestaurantList extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+};
 
-  render() {
-    return (
-      <div className="yelp-results-restaurants">
-        <select className="yelp-select-price" value={this.props.price} onChange={(event) => {
-          this.props.queryYelp({
-            price: event.target.value,
-            style: this.props.style,
-          });
-        }}>
-          <option value="1">*</option>
-          <option value="2">**</option>
-          <option value="3">***</option>
-          <option value="4">****</option>
-        </select>
-        <select className="yelp-select-style" value={this.props.style} onChange={(event) => {
-          this.props.queryYelp({
-            price: this.props.price,
-            style: event.target.value,
-          })
-        }}>
-          <option value="bars">Bars</option>
-          <option value="cafe">Cafe</option>
-          <option value="casual">Casual</option>
-          <option value="clubs">Clubs</option>
-          <option value="restaurant">Restaurant</option>
-        </select>
-        <ol>
-          {this.props.restaurants.map(value =>
-            <RestaurantListEntry restaurant={value} selectDestination={this.props.selectDestination}
-              saveDestination={this.props.saveDestination}
-            />,
-          )}
-        </ol>
-      </div>
-    );
-  }
-}
+const RestaurantList = props => (
+  <div className="yelp-results-restaurants">
+    <select className="yelp-select-price" value={props.price} onChange={(event) => {
+      props.queryYelp({
+        price: event.target.value,
+        style: props.style,
+      });
+    }}>
+      <option value="1">*</option>
+      <option value="2">**</option>
+      <option value="3">***</option>
+      <option value="4">****</option>
+    </select>
+    <select className="yelp-select-style" value={props.style} onChange={(event) => {
+      props.queryYelp({
+        price: props.price,
+        style: event.target.value,
+      })
+    }}>
+      <option value="bars">Bars</option>
+      <option value="cafe">Cafe</option>
+      <option value="casual">Casual</option>
+      <option value="clubs">Clubs</option>
+      <option value="restaurant">Restaurant</option>
+    </select>
+    <ol>
+      {props.restaurants.map(value =>
+        <RestaurantListEntry restaurant={value} selectDestination={props.selectDestination}
+          saveDestination={props.saveDestination}
+        />,
+      )}
+    </ol>
+  </div>
+);
 
 export default RestaurantList;
