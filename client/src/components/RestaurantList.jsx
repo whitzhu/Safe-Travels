3e5ef9e820fee799da5c2e1f776476b75b7fb75d
Gabriel Certeza <gabrielscerteza@gmail.com>
2017-03-24 14:24:51 -0700

    (refactor) add PropTypes and fix format

--- client/src/components/RestaurantList.jsx
+++ client/src/components/RestaurantList.jsx
@@ -2,28 +2,40 @@ import React, { PropTypes } from 'react';
 import RestaurantListEntry from './RestaurantListEntry';
 
 const propTypes = {
-
+  price: PropTypes.string.isRequired,
+  style: PropTypes.string.isRequired,
+  queryYelp: PropTypes.func.isRequired,
+  restaurants: PropTypes.array.isRequired,
+  saveDestination: PropTypes.func.isRequired,
 };
 
 const RestaurantList = props => (
   <div className="yelp-results-restaurants">
-    <select className="yelp-select-price" value={props.price} onChange={(event) => {
-      props.queryYelp({
-        price: event.target.value,
-        style: props.style,
-      });
-    }}>
+    <select
+      className="yelp-select-price"
+      value={props.price}
+      onChange={(event) => {
+        props.queryYelp({
+          price: event.target.value,
+          style: props.style,
+        });
+      }}
+    >
       <option value="1">*</option>
       <option value="2">**</option>
       <option value="3">***</option>
       <option value="4">****</option>
     </select>
-    <select className="yelp-select-style" value={props.style} onChange={(event) => {
-      props.queryYelp({
-        price: props.price,
-        style: event.target.value,
-      })
-    }}>
+    <select
+      className="yelp-select-style"
+      value={props.style}
+      onChange={(event) => {
+        props.queryYelp({
+          price: props.price,
+          style: event.target.value,
+        });
+      }}
+    >
       <option value="bars">Bars</option>
       <option value="cafe">Cafe</option>
       <option value="casual">Casual</option>
@@ -32,7 +44,9 @@ const RestaurantList = props => (
     </select>
     <ol>
       {props.restaurants.map(value =>
-        <RestaurantListEntry restaurant={value} selectDestination={props.selectDestination}
+        <RestaurantListEntry
+          restaurant={value}
+          selectDestination={props.selectDestination}
           saveDestination={props.saveDestination}
         />,
       )}
@@ -40,4 +54,5 @@ const RestaurantList = props => (
   </div>
 );
 
+RestaurantList.propTypes = propTypes;
 export default RestaurantList;
