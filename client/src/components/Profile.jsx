b546db6239253a02bd07e4cfa69c226b0b043d51
Gabriel Certeza <gabrielscerteza@gmail.com>
2017-03-24 13:59:49 -0700

    (refactor) add PropTypes and change to proper ES6 syntax for Profile.jsx

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import SavedTrip from './SavedTrip';
 
-const Profile = (props) => {
-  console.log(props.savedTrips);
-  return (<div>
+const propTypes = {
+  savedTrips: PropTypes.array.isRequired,
+};
+
+const Profile = props => (
+  <div>
     <h1>Saved Trips!</h1>
     {props.savedTrips.map(trip => (
       <SavedTrip trip={trip} />
     ))}
-  </div>);
-};
+  </div>
+);
 
+Profile.propTypes = propTypes;
 export default Profile;
