97cdb97aae33df392d538274d304498800d16921
Gary Wong <gary.wonghg@gmail.com>
2017-03-29 16:26:07 -0700

    Integrate Twilio to SafeTravel app Migrate seperate Twilio code to integrate with SafeTravel. Twilio now works for 1-way and 2-way messaging.

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const app = require('./server-config');
 const port = 8000;
 
 app.listen(process.env.PORT || port, () => {
-  /* eslint-disable no-console */
+  // eslint-disable no-console 
   console.log(`Server now listening on port ${port}`);
   /* eslint-enable no-console */
 });
