6d835ea62aba5264afb777cef516fe6d9dfafad8
whitzhu <whitzhu@gmail.com>
2017-04-01 07:31:55 -0700

    reactdnd example

--- client/src/components/Knight.jsx
+++ client/src/components/Knight.jsx
@@ -1,7 +1,45 @@
 import React, {Component} from 'react';
+import { DragSource } from 'react-dnd';
+import { ItemTypes } from './Constants';
 
-export default class Knight extends Component {
-  render() {
-    return <span>♘</span>;
+/**
+ * Specifies the drag source contract.
+ * Only `beginDrag` function is required.
+ */
+const knightSource = {
+  beginDrag() {
+    return {};
   }
 }
+  /**
+  * Specifies which props to inject into your component.
+  */
+function collect(connect, monitor) {
+  return {
+    connectDragSource: connect.dragSource(),
+    connectDragPreview: connect.dragPreview(),
+    isDragging: monitor.isDragging(),
+  };
+}
+
+class Knight extends Component {
+  render() {
+    const { connectDragSource, isDragging } = this.props;
+    return connectDragSource(
+      <div
+        style={{
+          fontSize: 40,
+          fontWeight: 'bold',
+          cursor: 'move',
+          opacity: isDragging ? 0.5 : 1,
+        }}
+      >
+        ♘
+      </div>,
+    );
+  }
+}
+
+export default DragSource(ItemTypes.KNIGHT, knightSource, collect)(Knight);
+
+
