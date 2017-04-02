7c869d7c03e16ba544b534efa3d832c802484c5c
whitzhu <whitzhu@gmail.com>
2017-04-01 20:48:55 -0700

    remove calcoltripentry address

--- client/src/components/CalColTripEntry.jsx
+++ client/src/components/CalColTripEntry.jsx
@@ -57,8 +57,6 @@ class CalColTripEntry extends Component {
               <p className=''>
                 <a href={trip.informationUrl} >{trip.name}</a>
               </p>
-              <p>Address: {trip.address}</p>
-              <p>{trip.city}, {trip.state} {trip.zipCode}</p>
             </div>
           </Col>
         </Row>
